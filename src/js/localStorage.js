5d5a17e6f834ecf3684caf814ab6d2af30e96c8b
3nergetik <carbo_r86@mail.ru>
2022-04-10 19:59:01 +0500

    2

---
+++ src/js/localStorage.js
@@ -0,0 +1,13 @@
+export function save() {
+  const container = document.body.querySelector('.container');
+  const html = container.innerHTML;
+  localStorage.setItem('state', JSON.stringify(html));
+}
+
+export function load() {
+  try {
+    return JSON.parse(localStorage.getItem('state'));
+  } catch (e) {
+    throw new Error('Ошибка загрузки');
+  }
+}
